16b304ea46250ec7ae31d96b8dadc6e0c70bdba5
Eben <arkoharkohaddoebenezer21@gmail.com>
2024-06-16 22:31:12 +0000

    test: matrix multiplication

--- Day3/src/Main.java
+++
@@ -1,9 +0,0 @@
-import EasyOnArrays.EasyOnArrays;
-
-public class Main {
-    public static void main(String[] args) {
-        EasyOnArrays easyOnArrays = new EasyOnArrays();
-        int[] arr = { 12, 13, 1, 10, 34, 1 };
-        easyOnArrays.printLargestThreeElements(arr);
-    }
-}
