2b890332507a58cbb7e440d15661baac7ed4a96c
Eben <arkoharkohaddoebenezer21@gmail.com>
2024-06-18 09:31:33 +0000

    refactor: moved day 2 into week 0

---
+++ Week0/Day2/Statistics/src/main/java/Statistics.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+// Author: Ebenezer Arkoh-Addo
+public class Statistics {
+    public static  void main(String[] args){
+        Scanner readInput = new Scanner(System.in);
+
+        System.out.print("Enter grades: ");
+        String[] userInput = readInput.nextLine().split(" ");
+        readInput.close();
+        statistics(userInput);
+    }
+
+    /**
+     * Calculates and displays statistics based on the given array of grades.
+     *
+     * @param scores An array of strings representing students scores.
+     */
+    public static void statistics(String[] scores){
+        int[] grades = new int[scores.length];
+        for (int i = 0; i < grades.length; i++) {
+            grades[i]= Integer.parseInt(scores[i]);
+        }
+        int minGrade = 100;
+        int maxGrade = 0;
+        double sum = 0;
+        for (int grade : grades) {
+            if (grade > maxGrade)
+                maxGrade = grade;
+            if (grade < minGrade)
+                minGrade = grade;
+            sum+=grade;
+        }
+        double averageGrade = sum/(grades.length);
+
+        int[] stats = new int[5];
+        for (int grade : grades) {
+            if (grade > 80) {
+                stats[4]++;
+            } else if (grade >= 61) {
+                stats[3]++;
+            } else if (grade >= 41) {
+                stats[2]++;
+            } else if (grade >= 21) {
+                stats[1]++;
+            } else {
+                stats[0]++;
+            }
+        }
+        int highestFrequency = 0;
+        for (int stat:stats){
+            if (stat>highestFrequency)
+                highestFrequency=stat;
+        }
+        System.out.println("Values:\n");
+        System.out.println("Maximum grade: " + maxGrade);
+        System.out.println("Minimum grade: " + minGrade);
+        System.out.println("Average grade: " + averageGrade);
+
+        System.out.println("Graph:\n");
+        for (int i = highestFrequency+2; i > 0; i--) {
+            System.out.print(i + "\t>\t");
+
+            for (int frequency : stats) {
+                if (frequency >= i) {
+                    System.out.print("####### \t");
+                } else {
+                    System.out.print("        \t");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("\t+------------+-----------+-----------+-----------+------------+");
+        System.out.println("\tI    0-20    I   21-40   I   41-60   I   61-80   I   81-100   I");
+
+    }
+
+}
