177c8c4707525f50ef290d60da5b02802b398bad
Eben <arkoharkohaddoebenezer21@gmail.com>
2024-07-01 12:59:35 +0000

    feat: shell sort implementation

--- week3/day14/ShellAndInsertionSort.java
+++ week3/day14/ShellAndInsertionSort.java
@@ -1,4 +1,4 @@
-public class InsertionSort {
+public class ShellAndInsertionSort {
 
 //     ALGORITHM insertionSort(array)
 // BEGIN
@@ -24,6 +24,23 @@ public class InsertionSort {
             }
 
             array[j + 1] = key;
+        }
+    }
+
+    public static void shellSort(int[] array) {
+        int n = array.length;
+
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i += 1) {
+                int temp = array[i];
+                int j;
+
+                for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+                    array[j] = array[j - gap];
+                }
+
+                array[j] = temp;
+            }
         }
     }
 
